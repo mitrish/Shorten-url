43160002359c2c41c9638f075971f387f8fb913c
Rishabh <rishabhmittal200@gmail.com>
2018-02-01 23:43:48 +0530

    Create Code.js

---
+++ Code.js
@@ -0,0 +1,62 @@
+// Include data for accessing Google APIs
+
+const apiKey = 'AIzaSyDEdLBGrYlstmNSHExjvvMjLdJ-LSBiaqk>';
+const url = 'https://www.googleapis.com/urlshortener/v1/url';
+
+// Some page elements
+
+const $inputField = $('#input');
+const $expandButton = $('#expand');
+const $shortenButton = $('#shorten');
+const $responseField = $('#responseField');
+
+// AJAX functions
+
+function expandUrl() {
+  const urlToExpand = url + 
+        '?key=' + apiKey +
+        '&shortUrl=' + $inputField.val();
+  const xhr = new XMLHttpRequest(); 
+  xhr.responseType = 'json';
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      $responseField.append('<p>Your expanded url is: </p><p>' + xhr.response.longUrl + '</p>');
+    }
+  };
+
+  xhr.open('GET', urlToExpand);
+  xhr.send();
+}
+
+function shortenUrl() {
+  const urlWithKey = url + '?key=' + apiKey;
+  const urlToShorten = $inputField.val();
+  const data = JSON.stringify({longUrl:urlToShorten});
+  const xhr = new XMLHttpRequest();
+  xhr.responseType='JSON';
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState === XMLHttpRequest.DONE){
+      console.log(xhr.response);
+    }
+  }
+xhr.Open('POST',urlWithKey);
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.send(data);
+}
+
+function expand() {
+  $responseField.empty();
+  expandUrl();
+  return false;
+}
+
+function shorten() {
+  $responseField.empty();
+  shortenUrl();
+  return false;
+}
+
+// Call functions on submit
+
+$expandButton.click(expand);
+$shortenButton.click(shorten);
